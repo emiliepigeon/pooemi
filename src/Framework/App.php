30a06d06e3c6e6ae5a8353ab6dfb65fb3633affe
emiliepigeon <emiliepigeon@free.fr>
2024-03-27 13:33:58 +0100

    second

---
+++ src/Framework/App.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Framework;
+
+use DI\ContainerBuilder;
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class App implements DelegateInterface
+{
+
+    /**
+     * List of modules
+     * @var array
+     */
+    private $modules = [];
+    /**
+     * @var string
+     */
+    private $definition;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var string[]
+     */
+    private $middlewares;
+
+    /**
+     * @var int
+     */
+    private $index = 0;
+
+    public function __construct(string $definition)
+    {
+
+        $this->definition = $definition;
+    }
+
+    /**
+     * Rajoute un module à l'application
+     *
+     * @param string $module
+     * @return App
+     */
+    public function addModule(string $module): self
+    {
+        $this->modules[] = $module;
+        return $this;
+    }
+
+    /**
+     * Ajoute un middleware
+     *
+     * @param string $middleware
+     * @return App
+     */
+    public function pipe(string $middleware): self
+    {
+        $this->middlewares[] = $middleware;
+        return $this;
+    }
+
+    public function process(ServerRequestInterface $request): ResponseInterface
+    {
+        $middleware = $this->getMiddleware();
+        if (is_null($middleware)) {
+            throw new \Exception('Aucun middleware n\'a intercepté cette requête');
+        } elseif (is_callable($middleware)) {
+            return call_user_func_array($middleware, [$request, [$this, 'process']]);
+        } elseif ($middleware instanceof MiddlewareInterface) {
+            return $middleware->process($request, $this);
+        }
+    }
+
+    public function run(ServerRequestInterface $request): ResponseInterface
+    {
+        foreach ($this->modules as $module) {
+            $this->getContainer()->get($module);
+        }
+        return $this->process($request);
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    private function getContainer(): ContainerInterface
+    {
+        if ($this->container === null) {
+            $builder = new ContainerBuilder();
+            $builder->addDefinitions($this->definition);
+            foreach ($this->modules as $module) {
+                if ($module::DEFINITIONS) {
+                    $builder->addDefinitions($module::DEFINITIONS);
+                }
+            }
+            $this->container = $builder->build();
+        }
+        return $this->container;
+    }
+
+    /**
+     * @return object
+     */
+    private function getMiddleware()
+    {
+        if (array_key_exists($this->index, $this->middlewares)) {
+            $middleware = $this->container->get($this->middlewares[$this->index]);
+            $this->index++;
+            return $middleware;
+        }
+        return null;
+    }
+}
